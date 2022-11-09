18179494717c5fdccbf0aff9f8d764eaac2c5404
Kwayiba <thato@iguardsa.net>
2022-11-09 14:32:07 +0200

    clean up

--- Integrated_Api/app/Home/__init__.py
+++
@@ -1,7 +0,0 @@
-# This bluepint will deal with all user management functionality
-
-from flask import Blueprint
-
-Home_Blueprint = Blueprint('Home', __name__, template_folder='templates')
-
-from . import views

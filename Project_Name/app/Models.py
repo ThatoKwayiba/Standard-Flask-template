1e6e66389cc1eff1c6b3e537e260e25333a1ef35
ThatoKwayiba <70237701+ThatoKwayiba@users.noreply.github.com>
2024-05-14 14:37:03 +0200

    Update Models.py

--- Project_Name/app/Models.py
+++ Project_Name/app/Models.py
@@ -8,6 +8,8 @@ class services(Base):
     __tablename__ = "table_name"
     id = Column(Integer, primary_key=True)
 
+passsword = 'not_password'
+ip_and_port = 'ip:port'
 
-engine = create_engine('postgresql://postgres:{password}@{ip_and_port}')
+engine = create_engine(f'postgresql://postgres:{password}@{ip_and_port}')
 Base.metadata.create_all(engine)
